c0a110880546039b5624f0f037a1dcee7c021367
daguolili <gsun@stevens.edu>
2017-04-23 15:17:35 -0400

    111

--- EE-553-2017S-master/HW8/HW8/main.cpp
+++ EE-553-2017S-master/HW8/HW8/main.cpp
@@ -1,79 +1,103 @@
+//Guoli Sun
+//10395608
 #include <iostream>
 using namespace std;
 
 class Matrix {
 private:
-    double* m; // points to a single vector of double...
+  double* m;
+  int rows;
+  int cols;
 public:
-    // write constructor, destructor, copy constructor, operator =
-    Matrix(int rows, int cols) : m(m) {
-        for(int i = 0; i < rows; i++ ){
-            for(int j = 0; j < cols; j++){
-                m[i][j] = 0;
-            }
-        }
+    Matrix(int rows, int cols) : rows(rows),cols(cols) {
+    double area[rows * cols];
+    int a2 = rows * cols;
+    m = new double[rows * cols];
+    for(int i =  0;i < a2; i++)
+      m[i] = 0;
     }
+  Matrix(int rows, int cols, double fill) : rows(rows),cols(cols) {
+    double area[rows * cols];
+    int a2 = rows * cols;
+    m = new double[rows * cols];
+    for(int i =  0; i < a2; i++)
+      m[i] = fill;
+  }
 
-    Matrix(int rows, int cols, double val) :m(m) {
-        for(int i = 0; i < rows; i++ ){
-            for(int j = 0; j < cols; j++){
-                m[i][j] = val;
-            }
-        }
+  Matrix(Matrix& orig) {
+    m = orig.m;
+    rows = orig.rows;
+    cols = orig.cols;
+    orig.m = nullptr;
+  }
+
+  ~Matrix(){
+    delete[] m;
+  }
+
+  Matrix(const Matrix& orig) : m(new double[orig.cols * orig.rows]),rows(orig.rows),cols(orig.cols){
+        for(int i = 0; i < cols*rows; i++)
+            m[i] = orig.m[i];
     }
 
     double  operator ()(int i, int j) const {
-        return m[i][j];
+        return m[i * this->cols + j];
     }
+
     double&  operator ()(int i, int j) {
-        return m[i][j];
-    }
-    Matrix(Matrix&& orig) {
-    m = orig.m;
-        rows = orig.rows;
-        cols = orig.cols;
-        orig.m = nullptr;
+    return m[i * this->cols + j];
     }
 
-    friend ostream& operator <<(ostream& s, const Matrix& str){
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                cout << m[i][j] <<' ';
-            }
-            cout <<'\n';
+  void operator =(const Matrix& orig){
+      delete [] m;
+      m = new double[orig.rows * orig.cols];
+      rows = orig.rows;
+      cols = orig.cols;
+      for (int i = 0; i < rows * cols; i++)
+        m[i] = orig.m[i];
+    }
+
+  friend Matrix operator +(const Matrix& a, const Matrix& b) {
+    if (a.cols != b.cols && a.rows != b.rows){
+      cout << "the size of these matrices is not equal." << endl;
+      return a;
+    }
+
+    else{
+        Matrix ans(a.rows, a.cols);
+      for(int i = 0; i < a.rows; i++){
+        for(int j =0; j < a.cols; j++){
+          ans.m[i * ans.cols + j] = a.m[i * ans.cols  + j] + b.m[i*ans.cols + j];
         }
-        return s;
+      }
+          return ans;
+    }
     }
 
-    friend Matrix operator +(const Matrix& a, const Matrix& b) {
-        // test if the two matrices are equal in size!!!
-        if(a.i != b.i || a.j != b.j){
-            cout << "a,b are not same size!" << '\n';
-            break;
-        }
-        Matrix ans(a.i + b.i, a.j + b.j);
-        return ans;
+  friend ostream& operator << (ostream& s, const Matrix& ref){
+    for(int i = 0; i < ref.rows; i++){
+      s << '\n';
+      for(int j = 0; j < ref.cols; j++){
+        s << ref.m[i * ref.cols + j] << " ";
+      }
     }
+    return s;
+  }
 
-    //friend Matrix operator =(const )
-    void operator =(const Matrix& a, const Matrix& b) {
-        Matrix ans(a.operator (i)(j) = b.operator (i)(j));
-    }
 };
-
 
 int main() {
     Matrix m1(3, 4); // zeros
     Matrix m2(2, 3, 1.5); // fill with 1.5
-    cout << m1 << '\n';
+  cout << m1 << '\n';
     /*
         0   0   0   0
-        0   0   0   0
-        0   0   0   0
+    0   0   0   0
+    0   0   0   0
     */
     cout << m1(0, 1) << '\n';
     m1(0,1) = 5.5;
-    Matrix m3 = m1 + m1;
+  Matrix m3 = m1 + m1;
     Matrix m4 = m3;  //copy constructor
     cout << m4 << '\n';
     m4(1,2) = 11.2;
